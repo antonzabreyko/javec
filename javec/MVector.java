ba6bee0b5f352b772f77ac027330c92ab9f5fbc4
antonzabreyko <anton.zabreyko@gmail.com>
2020-04-07 19:13:53 -0700

    Added several methods to MVector

--- javec/MVector.java
+++ javec/MVector.java
@@ -9,14 +9,40 @@ public class MVector<A>
   /** Object representation of a vector. Based on an array. */
 
   private Object[] vec;
+  private boolean transpose;
 
   /** Constructor that takes in an arbitrary number of args and sets up a vector. */
   public MVector(A... args)
   {
     vec = new Object[args.length];
+    transpose = false;
 
     for(int i = 0; i<args.length; i++) {
       vec[i] = args[i];
     }
+  }
+
+  /**Alternative constructor that takes in both arbitrary number of args and a variable determining whether or not the
+   * vector is transposed.
+   */
+  public MVector(boolean transpose, A... args) {
+
+    vec = new Object[args.length];
+    this.transpose = transpose;
+
+    for(int i = 0; i<args.length; i++) {
+      vec[i] = args[i];
+    }
+
+  }
+
+  /** Returns value at INDEX. */
+  public A get(int index) {
+    return (A)vec[index];
+  }
+
+  /** Changes the transpose of the vector. */
+  private void setTranspose() {
+    transpose = !transpose;
   }
 }
