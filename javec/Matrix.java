22a6dba24550fb500579d4f17ef1518b4f54e8a2
antonzabreyko <anton.zabreyko@gmail.com>
2020-04-10 23:55:11 -0700

    Changing internal structure of matrix

--- javec/Matrix.java
+++ javec/Matrix.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 public class Matrix<A extends Info> {
 
     /** ArrayList representation of the matrix. */
-    private ArrayList<MVector<A>> mat;
+    private ArrayList<A[]> mat;
     private int dimension = 0;
 
     /**Default constructor for matrix, creates an empty matrix. */
     public Matrix() {
-        mat = new ArrayList<MVector<A>>();
+        mat = new ArrayList<A[]>();
         dimension = 0;
     }
 
@@ -35,4 +35,6 @@ public class Matrix<A extends Info> {
         MVector<A> v = new MVector<A>(arr);
         return add(v);
     }
+
+
 }
